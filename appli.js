d85240817f2ef1103af5d717c344b44aae9a1b54
camer <128632619+judicael10@users.noreply.github.com>
2023-08-20 01:44:35 +0100

    Add files via upload

---
+++ appli.js
@@ -0,0 +1,52 @@
+function show(){
+    document.querySelector('.sou-menue').classList.toggle('active-sous-menue')
+    document.queryselector('.navigation').classList.toggle('open')
+}
+
+
+
+function show2(){
+    document.querySelector('.navigation').classList.toggle('open')
+    document.querySelector('.oliver').classList.toggle('bres')
+}
+
+function show3(){
+    document.querySelector('.chiox').classList.toggle('open2')
+    document.querySelector('body').classList.toggle('bodyy')
+    document.querySelector('.navigation').classList.toggle('bodyy')
+}
+
+function shownew(){
+    document.querySelector('.action').classList.toggle('actionof')
+}
+function shownew1(){
+    document.querySelector('.action1').classList.toggle('actionof')
+}
+
+function shownew2(){
+    document.querySelector('.action2').classList.toggle('actionof')
+}
+
+function shownew3(){
+    document.querySelector('.action3').classList.toggle('actionof')
+}
+
+
+function shownew4(){
+    document.querySelector('.action4').classList.toggle('actionof')
+}
+
+
+function shownew5(){
+    document.querySelector('.action5').classList.toggle('actionof')
+}
+
+
+
+//.................................
+
+// let btnback = document.querySelector('.back');
+
+// btnback.addEventListener('click', ()=>{
+//     window.history.back();
+// })
